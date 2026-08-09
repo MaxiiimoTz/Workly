a9689caa4eac7ceabf018c0c0c2b2687f7fa94cf
Maximo <maxiiimo.tz@gmail.com>
2026-08-09 03:36:35 -0500

    Connect frontend to production backend

--- frontend/src/pages/Dashboard/DashboardPage.tsx
+++ frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -12,7 +12,7 @@ import {
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: ""https://workly-ilqb.onrender.com/api"",
+    baseURL: "https://workly-ilqb.onrender.com/api",
 });
 
 interface RecordItem {
